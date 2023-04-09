582c2154398b434bf1b4b470d65f05a71ccf8112
msaifull <saifulguardian92@gmail.com>
2023-04-09 10:15:44 +0700

    setup express js

--- app/api/v1/users/controller.js
+++ app/api/v1/users/controller.js
@@ -1,7 +1,9 @@
-const getAllusers= async(req, res, next)=>{
+const getAllUsers= async(req, res, next)=>{
     try {
-        
+        res.send('respond with a resource');
     } catch (err) {
-        
+        next(err)
     }
 }
+
+module.exports = {getAllUsers}
--- app/api/v1/users/router.js
+++ app/api/v1/users/router.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { getAllUsers } = require('./controller');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/users',getAllUsers )
 
 module.exports = router;
